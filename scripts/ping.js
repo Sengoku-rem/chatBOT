921d0fa4700458a6eadd41642bc64d5cb4a53837
Sengoku-rem <103261281+Sengoku-rem@users.noreply.github.com>
2023-07-17 13:49:13 +0900

    aa

--- scripts/ping.js
+++ scripts/ping.js
@@ -1,13 +1,39 @@
-// Description:
-//   Utility commands surrounding Hubot uptime.
-//
-// Commands:
-//   ping - Reply with pong
-//   echo <text> - Reply back with <text>
-//   time - Reply with current time
-'use strict';
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
 
 module.exports = (robot) => {
+  robot.respond(/join (.*)/i, (res) => {
+    const notes = res.match[1].trim().toLowerCase().split(' ');
+
+    // 音階に対応するファイルパスのマップ
+    const noteFilePaths = {
+      'c': '/path/to/c.mp3',
+      'd': '/path/to/d.mp3',
+      'e': '/path/to/e.mp3',
+      // 他の音階に対応するファイルパスを追加
+    };
+
+    // ファイルパスの配列
+    const filePaths = notes.map((note) => noteFilePaths[note]).filter(Boolean);
+
+    // ファイルの結合
+    const joinedFilePath = path.join(__dirname, '../../Muu/joined.mp3');
+    const ffmpegPath = '/usr/bin/ffmpeg'; // FFmpegのパスを指定
+
+    const command = `${ffmpegPath} -i "concat:${filePaths.join('|')}" -c copy ${joinedFilePath}`;
+
+    exec(command, (error) => {
+      if (error) {
+        console.error('音声データの結合中にエラーが発生しました:', error);
+        res.send('音声データの結合中にエラーが発生しました');
+      } else {
+        res.send(`MP3データの結合と保存が完了しました: ${joinedFilePath}`);
+      }
+    });
+  });
+
+  // 以下、既存のコマンドの定義
   robot.respond(/PING$/i, (res) => {
     res.send('PONG');
   });
